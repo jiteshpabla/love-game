c35820d28a84f4a53db7e3b794b188093ceba06c
Jitesh Pabla <jiteshpabla97@gmail.com>
2018-06-05 23:26:17 +0530

    Add initial configuration

---
+++ conf.lua
@@ -0,0 +1,7 @@
+function love.conf(t)
+	-- define the dimentions of the game window
+	t.window.width = 1280
+	t.window.height = 720
+	-- for showing errors in the love window
+	t.console = true
+end
